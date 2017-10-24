69dee04918eb734e9b5c8c6923ae08b3053101ec
Nathan Stewart <stewart.nathang@gmail.com>
2017-10-24 15:09:51 -0700

    initial commit, config and render text to sim for albums react-native app

---
+++ App.js
@@ -0,0 +1,13 @@
+// Code for IOS
+
+// Import a library to help create a component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Create component
+export default App = () => (  // Now exporting to index.js
+    <Text>Some Text</Text>
+  );
+
+// Render it to the device
+// AppRegistry.registerComponent('albums', () => App); This is now being imported from index.js
